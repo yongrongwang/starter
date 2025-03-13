yongrongwang/nvchad-starter

Add LSP for markdown and javascript, update UI config

lua/options.lua
@@ -2,5 +2,16 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+local bo = vim.bo
+o.cursorlineopt ='both' -- to enable cursorline!
+o.relativenumber = true
+o.signcolumn = "auto"
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "html" },
+  callback = function()
+    bo.shiftwidth = 4
+    bo.tabstop = 4
+    bo.softtabstop = 4
+  end
+})

yongrongwang/starter

lua/options.lua
@@ -2,5 +2,19 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+local bo = vim.bo
+o.cursorlineopt ='both' -- to enable cursorline!
+o.relativenumber = true
+o.signcolumn = "auto"
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "lua" },
+  callback = function()
+    bo.shiftwidth = 2
+    bo.tabstop = 2
+    bo.softtabstop = 2
+  end
+})